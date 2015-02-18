3affc4f34202cc482d3a6f404ad266e248ea44f0
bkerefeyski <bkerefeyski@gmail.com>
2015-02-18 21:35:41 +0200

    C#2

---
+++ C# 2 Homeworks/1.Arrays/IndexOfLetters/IndexLetters.cs
@@ -0,0 +1,43 @@
+﻿//Problem 12. Index of letters
+
+//Write a program that creates an array containing all letters from the alphabet (A-Z).
+//Read a word from the console and print the index of each of its letters in the array.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexOfLetters
+{
+    class IndexLetters
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word:");
+            string word = Console.ReadLine();
+            char[] alphabet = new char[26];
+            for (int i = 0; i < alphabet.Length ; i++)
+            {
+                alphabet[i] = (char)(i + 65);
+            }
+            List<char> letters = new List<char>(alphabet);
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j < letters.Count; j++)
+                {
+                    if(word[i] == letters[j])
+                    {
+                        Console.WriteLine(j);
+                    }
+                }
+            }
+            
+
+            
+
+        }
+    }
+}
